98645862378255c0ae4a0c9f4d41d939d94adfc0
Michael Maguire <mick@javascript-spreadsheet-programming.com>
2021-10-01 20:06:49 +0100

    Basic implementation done
    Basic working VCF2TSV parser implemented

--- vcf2tsv.py
+++ vcf2tsv.py
@@ -12,7 +12,8 @@ class VCF2TSV:
         self.vcf_file_path = vcf_file_path
         if not os.path.exists(self.vcf_file_path):
             raise IOError('Given file "{}" does not exist!'.format(self.vcf_file_path))
-    
+        self.info_schema = self.generate_info_schema()
+
     def write_header_to_file(self, output_file):
         """
 
@@ -58,6 +59,7 @@ class VCF2TSV:
         info_rows = self.get_info_rows()
         regex_pat = r'[<](.+),Description="'
         info_schema = {}
+        column_index = 0 # Added to inner dicts to file set output column values for info entries
         for info_row in info_rows:
             match = re.search(regex_pat, info_row)
             if match:
@@ -65,8 +67,34 @@ class VCF2TSV:
                 info_schema_element = dict(entry.split("=") for entry in matched_info.split(','))
                 element_id = info_schema_element['ID']
                 info_schema_element.pop('ID') 
+                info_schema_element['column_index'] = column_index
+                column_index +=1
                 info_schema[element_id] = info_schema_element
         return info_schema
+
+    def get_info_column_names_in_order(self):
+        """
+        """
+        info_names = list(self.info_schema.keys())
+        info_names_in_order = sorted(info_names, 
+                                     key=lambda info_name: self.info_schema[info_name]['column_index'])
+        return info_names_in_order
+
+    def parse_info_column(self, info_column_value):
+        """
+        """
+        info_elements = info_column_value.strip().split(';')
+        key, val = None, None
+        extracted_values = [None] * len(list(self.info_schema.keys()))
+        for info_element in info_elements:
+            if '=' in info_element:
+                key, val = info_element.split('=')
+            else:
+                key, val = info_element, 'X'
+            column_position = self.info_schema[key]['column_index']
+            extracted_values[column_position] = val
+        return extracted_values
+            
 
 if __name__ == '__main__':
     from pprint import pprint
@@ -78,3 +106,5 @@ if __name__ == '__main__':
     #vcf2tsv.write_header_to_file(header_file_path)
     #vcf2tsv.write_body_to_file(body_file_path)
     pprint(vcf2tsv.generate_info_schema())
+    pprint(vcf2tsv.parse_info_column('dbSNP_154;TSA=indel;E_Freq;E_1000G;E_TOPMed;AFR=0.4909;AMR=0.3602;EAS=0.3363;EUR=0.4056;SAS=0.4949'))
+    pprint(vcf2tsv.get_info_column_names_in_order())
