febd7ac9bb4495e10f7c51424190345d5a7988b2
Michael Maguire <mick@javascript-spreadsheet-programming.com>
2021-10-07 11:07:47 +0100

    Added documentation
    Added documentation

--- files2sqlite.sh
+++ files2sqlite.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Given four arguments (checks for this):
+# 1. The name of the SQLite DB - full path or it will be created in the directory where the 
+#   script is run
+# 2., 3., and 4. The names of the file contaning the variant detals, the the INFO key-val pairs 
+#   and the INFO flags
+# Warning: Removes the SQLite file of that name if it already exists!
+# Uses a HEREDOC to execute a series of SQLite commands to:
+# - Set the mode to tabs (assuming the input files are tab-delimited!)
+# - Set headers on
+# - Execute an external SQL DDL script to create the tables
+# - Three import statements import the files into the tables just created
+# - Create the indexes
+# Examle invocation:  ~/mybix/files2sqlite.sh sample_1k_vcf.db 1000GENOMES-phase_3_1k_sample_variant_details.txt 1000GENOMES-phase_3_1k_sample_info_keys_vals.txt 1000GENOMES-phase_3_1k_sample_info_flags.txt
+
 if [ "$#" -ne 4 ]; then
     echo "Error! Expected two arguments got $# Exiting!"
     exit 1
