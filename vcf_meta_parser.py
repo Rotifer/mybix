fc5c0ff33263ea06041d69f1e6fa8f1fa4b48a5e
Michael Maguire <mick@javascript-spreadsheet-programming.com>
2021-10-08 17:21:57 +0100

    Added a table generator for INFO
    Added a table generator for INFO

--- vcf_meta_parser.py
+++ vcf_meta_parser.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 class VCFMetaParser:
     """
@@ -10,10 +11,11 @@ class VCFMetaParser:
         """"""
         self.vcf_file_path = vcf_file_path
         self.output_dir = output_dir
+        self.info_properties = ['ID', 'Number', 'Type', 'Description']
         self.metadata_lines = self.get_metadata_lines()
 
     def get_metadata_lines(self):
-        """Return a list of metadata lines in the VCF file"""
+        """Return a list of metadata lines, that is, those beginning with ##, in the VCF file"""
         metadata_lines = []
         with open(self.vcf_file_path, 'rt') as fh:
             for line in fh:
@@ -23,9 +25,37 @@ class VCFMetaParser:
                     metadata_lines.append(line.strip(os.linesep))
             return metadata_lines
 
+    def create_info_maps(self):
+        """Extract INFO metadata from the metadata lines and return them as a list of dictionaries"""
+        info_maps = []
+        info_lines = [md_line for md_line in self.metadata_lines 
+                        if md_line.startswith('##INFO=')]
+        for info_line in info_lines:
+            info_map = {}
+            for info_property in self.info_properties:
+                re_pat = '{}=([^,>"]*)'.format(info_property)
+                match = re.search(re_pat, info_line)
+                if match:
+                    info_val = match.group(1)
+                    info_map[info_property] = info_val
+            info_maps.append(info_map)
+        return info_maps
+
+    def convert_info_maps_to_table(self):
+        """"""
+        info_maps = self.create_info_maps()
+        info_table_column = {}
+        for info_property in self.info_properties:
+            info_table_column[info_property] = []
+            for info_map in info_maps:
+                info_table_column[info_property].append(info_map[info_property])
+        return info_table_column
+
 if __name__ == '__main__':
     from pprint import pprint
     dir_path = '{}/big_files/'.format(os.environ['HOME']) 
-    vcf_file_path = os.path.join(dir_path, '1000GENOMES-phase_3.vcf')
+    vcf_file_path = os.path.join(dir_path, 'UKB_WGS_graphtyper_SVs_150k_sites.vcf')
     vcf_meta_parser = VCFMetaParser(vcf_file_path, dir_path)
-    pprint(vcf_meta_parser.get_metadata_lines())
+    #pprint(vcf_meta_parser.get_metadata_lines())
+    info_table = vcf_meta_parser.convert_info_maps_to_table()
+    pprint(info_table)
